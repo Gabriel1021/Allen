b143d1165d566b1cc6529f66975cd3d32407bd19
zouxiaoxiang <zouxiaoxiang@zhengbang.com>
2021-05-08 17:15:22 +0800

    设计模式

---
+++ src/main/java/com/gabriel/service/BigTank.java
@@ -0,0 +1,15 @@
+package com.gabriel.service;
+
+/**
+ * @author : zouxiaoxiang
+ * @description :
+ * @date : 2021.05.08 9:43
+ */
+public class BigTank extends Tank{
+
+  @Override
+  public void move() {
+
+  }
+
+}
---
+++ src/main/java/com/gabriel/service/Tank.java
@@ -0,0 +1,10 @@
+package com.gabriel.service;
+
+/**
+ * @author : zouxiaoxiang
+ * @description :
+ * @date : 2021.05.08 9:41
+ */
+public abstract class Tank {
+    public abstract void move();
+}
