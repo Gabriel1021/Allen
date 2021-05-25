d8d05a0c4a6bfb7aefa8086016a7225a69e6e6d4
zouxiaoxiang <zouxiaoxiang@zhengbang.com>
2021-05-25 16:17:32 +0800

    cglib代理

--- src/main/java/com/gabriel/design/proxy/cglib/Client.java
+++ src/main/java/com/gabriel/design/proxy/cglib/Client.java
@@ -3,6 +3,7 @@ package com.gabriel.design.proxy.cglib;
 import com.gabriel.service.Tank;
 import java.lang.reflect.Method;
 import java.util.Random;
+import org.springframework.cglib.core.DebuggingClassWriter;
 import org.springframework.cglib.proxy.Enhancer;
 import org.springframework.cglib.proxy.MethodInterceptor;
 import org.springframework.cglib.proxy.MethodProxy;
@@ -15,6 +16,8 @@ import org.springframework.cglib.proxy.MethodProxy;
 public class Client {
 
   public static void main(String[] args) {
+
+    System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, "G:\\Code\\github");
     //1. 创建一个工具类
     Enhancer enhancer = new Enhancer();
     //2. 设置父类
@@ -42,7 +45,10 @@ class LogMethodInterceptor implements MethodInterceptor {
   @Override
   public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
 
-    System.out.println(o.getClass().getSuperclass().getName());
+    System.out.println("生成的代理类："+o.getClass().getName());
+
+    System.out.println("被代理类继承的父类："+o.getClass().getSuperclass().getName());
+
     before();
     Object result = null;
     result = methodProxy.invokeSuper(o, objects);
